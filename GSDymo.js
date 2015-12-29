d87a88c8ac3d7901f414cf7877d552491cd0e37a
Jordi Puigdellívol <jordi@gloobus.net>
2015-12-29 18:31:00 +0100

    Starting with jquery plugin

--- GSDymo.js
+++ GSDymo.js
@@ -4,3 +4,88 @@
 
 var DYMO_ORIENTATION_LANDSCAPE = 'Landscape';
 var DYMO_ORIENTATION_PORTRAIT  = 'Portrait';
+
+
+//-----------------------
+// TICKET EDITOR PLUGIN
+//-----------------------
+;(function ( $ ) {
+
+    //========================================================================
+    // INIT
+    //========================================================================
+    $.fn.dymoEditor = function(options) {
+        //-----------
+        // Default options
+        //-----------
+
+        this.settings   = $.extend({}, $.fn.dymoEditor.defaults, options);
+        var settings    = this.settings;
+
+        //-----------
+        //Private variables
+        //-----------
+        var editor;
+
+        //-----------
+        //Functions
+        //-----------
+        this.toggleOrientation  = function (type) {            toggleOrientation(this);         return this; }
+        this.exportXML          = function (type) {            exportXML(this);                 return this; }
+
+        //-----------
+        // Init
+        //-----------
+        init(this);
+        return this;
+    }
+
+
+    //========================================================================
+    // Plugin defaults – added as a property on our plugin function.
+    //========================================================================
+    $.fn.dymoEditor.defaults = {
+        width:  "250px",
+        height: "100px",
+    };
+
+    //========================================================================
+    // INTI
+    //========================================================================
+    function init(element){
+        element.editor = $('#dymoEditor');
+    }
+
+    //========================================================================
+    // FUNCTIONS
+    //========================================================================
+    function toggleOrientation(element){
+        console.log("Toggle Orientation");
+        var oldHeight   = element.editor.css('height');
+        var oldWidth    = element.editor.css('width');
+
+        element.editor.animate({
+            height  : oldWidth,
+            width   : oldHeight
+        }, 1000, function() {
+            // Animation complete.
+        });
+    }
+
+    function exportXML(element){
+        var xml = '<?xml version="1.0" encoding="utf-8"?>' +
+                  '<DieCutLabel Version="8.0" Units="twips">';
+
+        xml = xml + '<PaperOrientation>Landscape</PaperOrientation>'+
+                    '<Id>Address</Id>' +
+                    '<PaperName>30252 Address</PaperName>';
+
+
+        xml = xml + '</DieCutLabel>';
+
+        console.log(xml);
+    }
+
+
+
+})( jQuery );
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Dymo Editor
+
+A JQuery Plugin to design and print Dymo labels
+
+Right now, the div element to handle the editor must have as ID `dymoEditor`.
