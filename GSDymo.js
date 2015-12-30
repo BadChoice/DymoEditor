a0c979cc9175fc349ae55b506029da4bfe4264d3
Jordi Puigdellívol <jordi@gloobus.net>
2015-12-30 12:44:22 +0100

    Added barcode object

--- GSDymo.js
+++ GSDymo.js
@@ -7,14 +7,14 @@
  * (c) 2016 - Revo Systems http://revo.works
  */
 
-var DYMO_ORIENTATION_LANDSCAPE = 'Landscape';
-var DYMO_ORIENTATION_PORTRAIT  = 'Portrait';
+var DYMO_ORIENTATION_LANDSCAPE  = 'Landscape';
+var DYMO_ORIENTATION_PORTRAIT   = 'Portrait';
 
-var DYMO_PAPER_ADDRESS      = "30252 Address";
-var DYMO_PAPER_FILEFOLDER_  = "30327 File Folder – offset";
-var DYMO_PAPER_SHIPPING     = "30256 Shipping";
+var DYMO_PAPER_ADDRESS          = "30252 Address";
+var DYMO_PAPER_FILEFOLDER       = "30327 File Folder – offset";
+var DYMO_PAPER_SHIPPING         = "30256 Shipping";
 
-var DYMO_PIXEL_TO_INCH      = 20;
+var DYMO_PIXEL_TO_INCH          = 12;
 
 //-----------------------
 // TICKET EDITOR PLUGIN
@@ -71,8 +71,8 @@ var DYMO_PIXEL_TO_INCH      = 20;
     //========================================================================
     function init(element){
         element.editor = $('#dymoEditor');
-        element.editor.css('width','300px');
-        element.editor.css('height','150px');
+        element.editor.css('width','450px');
+        element.editor.css('height','235px');
 
         createPopup(element);
     }
@@ -123,6 +123,12 @@ var DYMO_PIXEL_TO_INCH      = 20;
             object.css('width' ,'100px');
             object.css('height','50px');
         }
+        else if(type == 'barcode'){
+            object = $('<div type="barcode" class="dymoEditorBarcodeObject"><div>|| | |||| ||| | |||| ||| | |||| ||| | |||| </div></div>');
+            object.css('width' ,'300px');
+            object.css('height','50px');
+            object.attr('text','barcode')
+        }
 
         object.addClass ('dymoEditorObject');
         object.draggable({containment: "parent", grid: [ 5, 5 ]});
@@ -135,7 +141,14 @@ var DYMO_PIXEL_TO_INCH      = 20;
     }
 
     function getObjectText(object){
-        return object.children().first().text();
+        var type = object.attr('type');
+        if(type == 'text') {
+            return object.children().first().text();
+        }
+        else if(type == 'barcode'){
+            return object.attr('text');
+        }
+        return "";
     }
 
     function updateObjectText(element){
@@ -146,6 +159,9 @@ var DYMO_PIXEL_TO_INCH      = 20;
         }
         else if(objectType == "value"){
             element.currentObject.children().first().text("{" + $('#popupSelect').val() + "}");
+        }
+        else if(objectType == 'barcode'){
+            element.currentObject.attr('text',$('#popupInput').val());
         }
     }
 
@@ -164,13 +180,21 @@ var DYMO_PIXEL_TO_INCH      = 20;
         var objectType      = object.attr("type");
         var popupInput      = $('#popupInput').hide();
         var popupSelect     = $('#popupSelect').hide();
+        var popupSave       = $('#popupSave').hide();
 
         if(objectType == 'text') {
             popupInput.show();
             popupInput.val(getObjectText(object));
+            popupSave.show();
         }
         else if(objectType == 'value') {
             popupSelect.show();
+            popupSave.show();
+        }
+        else if(objectType == 'barcode'){
+            popupInput.show();
+            popupInput.val(getObjectText(object));
+            popupSave.show();
         }
 
     }
@@ -202,7 +226,9 @@ var DYMO_PIXEL_TO_INCH      = 20;
 
     function exportObjectXml(object){
 
-        var x       = (object.position().left - object.parent().position().left)* DYMO_PIXEL_TO_INCH + 150;
+        var type    = object.attr("type");
+
+        var x       = (object.position().left - object.parent().position().left)* DYMO_PIXEL_TO_INCH + 160;
         var y       = (object.position().top  - object.parent().position().top) * DYMO_PIXEL_TO_INCH + 100;
         var width   = object.width()         * DYMO_PIXEL_TO_INCH;
         var height  = object.height()        * DYMO_PIXEL_TO_INCH;
@@ -212,9 +238,11 @@ var DYMO_PIXEL_TO_INCH      = 20;
         console.log("Parent position:" + object.parent().position().left + " - " + object.parent().position().top );
         console.log("Final  position:" + (object.position().left - object.parent().position().left) + " - " + (object.position().top - object.parent().position().top) + "-" + width + "-" + height);
         console.log("Final. position:" + x + " - " + y + "-" + width + "-" + height);
-        console.log("---")
+        console.log("---");
 
-        return '<ObjectInfo>\
+
+        if(type == 'text') {
+            return '<ObjectInfo>\
                     <TextObject>\
                         <Name>Text</Name>\
                         <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />\
@@ -230,19 +258,41 @@ var DYMO_PIXEL_TO_INCH      = 20;
                         <Verticalized>False</Verticalized>\
                         <StyledText>\
                             <Element>\
-                                <String>'+ text +'</String>\
+                                <String>' + text + '</String>\
                                     <Attributes>\
-                                        <Font Family="Arial" Size="12" Bold="False" Italic="False"\
-                                             Underline="False" Strikeout="False" />\
+                                        <Font Family="Arial" Size="12" Bold="False" Italic="False" Underline="False" Strikeout="False" />\
                                         <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />\
                                     </Attributes>\
                             </Element>\
                         </StyledText>\
                     </TextObject>\
-                    <Bounds X="' + x + '" Y="' + y + '" Width="'+width+'" Height="'+height+'" />\
+                    <Bounds X="' + x + '" Y="' + y + '" Width="' + width + '" Height="' + height + '" />\
                 </ObjectInfo>';
+        }
+        else if(type == 'barcode'){
+            return '<ObjectInfo>\
+                    <BarcodeObject>\
+                        <Name>Barcode</Name>\
+                        <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />\
+                        <BackColor Alpha="0" Red="255" Green="255" Blue="255" />\
+                        <LinkedObjectName>BarcodeText</LinkedObjectName>\
+                        <Rotation>Rotation0</Rotation>\
+                        <IsMirrored>False</IsMirrored>\
+                        <IsVariable>True</IsVariable>\
+                        <Text>' + text + '</Text>\
+                        <Type>Code128Auto</Type>\
+                        <Size>Medium</Size>\
+                        <TextPosition>Bottom</TextPosition>\
+                        <TextFont Family="Arial" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False" />\
+                        <CheckSumFont Family="Arial" Size="8" Bold="False" Italic="False" Underline="False" Strikeout="False" />\
+                        <TextEmbedding>None</TextEmbedding>\
+                        <ECLevel>0</ECLevel>\
+                        <HorizontalAlignment>Center</HorizontalAlignment>\
+                        <QuietZonesPadding Left="0" Top="0" Right="0" Bottom="0" />\
+                    </BarcodeObject>\
+                    <Bounds X="' + x + '" Y="' + y + '" Width="' + width + '" Height="' + height + '" />\
+                </ObjectInfo>';
+        }
     }
-
-
 
 })( jQuery );
